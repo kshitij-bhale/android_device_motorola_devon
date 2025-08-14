92e8c668f8c1319afc390aa9c6b33638a2276d4f
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-08-14 17:57:03 +0530

    devon: Axion bringup

--- lineage_devon.mk
+++ lineage_devon.mk
@@ -26,3 +26,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="devon_g-user 13 T2SNS33.73-22-3-15 f9ee6-5bd7d release-keys" \
     BuildFingerprint=motorola/devon_g/devon:13/T2SNS33.73-22-3-15/f9ee6-5bd7d:user/release-keys \
     DeviceProduct=devon_g
+
+# Axion Flags
+TARGET_ENABLE_BLUR := true
+AXION_MAINTAINER := Kshitij
